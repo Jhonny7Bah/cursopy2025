123bbf40b48f57bcfae9cc5f8becfb344efe47b1
Jhonny7Bah <jhonappsh@gmail.com>
2025-10-16 08:41:49 -0300

    mixinFile

--- cursoPyModulo3/modulo3.0(recomeco).py
+++ cursoPyModulo3/modulo3.0(recomeco).py
@@ -1246,7 +1246,6 @@ print(quatro.mro()) # [<class '__main__.quatro'>, <class '__main__.dois'>, <clas
 
 # pair programing -> programação guiada
 ###############################################################################
-###############################################################################
 # POO - Abstração.
 ## Abstração é o ato de focar no que o objeto faz, e não em como ele faz. 
 # Nesse sentido, reduzimos a complexidade mostrando apenas o que é essencial para o uso.
@@ -1261,10 +1260,18 @@ print(quatro.mro()) # [<class '__main__.quatro'>, <class '__main__.dois'>, <clas
 
 # Classe base (abstrata)
 class Log:
-    def log(self, msg):
+    def log(self, msg): 
         # Esse método serve apenas como modelo para as subclasses.
         raise NotImplementedError('Você não deve usar essa classe diretamente. Use a classe filha.')
-
+    
+    # método de error que vai retornar o método log com o Erro
+    def log_error(self, msg):
+        return self.log(f'Error: {msg}')
+    
+    # método de suceso que vai retornar o método log com o sucesso
+    def log_sucess(self, msg):
+        return self.log(f'Sucess: {msg}')
+    
 # Classe concreta que implementa a abstração
 class LogMixin(Log):
     def log(self, msg):
@@ -1273,9 +1280,49 @@ class LogMixin(Log):
 # Agora, LogMixin implementa o comportamento definido em Log,
 # escondendo a complexidade e exibindo apenas o necessário.
 LogMixin().log('Sistema iniciado!')
+LogMixin().log_error('Deu pau')
+LogMixin().log_sucess('Deu tudo certo')
 
 # mas caso você tente acessar log diretamente, perceba que vai dar erro:
 try:
     Log().log('olaa')
 except:
     print('Se isso for executado, então deu erro.')
+
+#########
+# Agora, vamos utilizar a classe que criamos para realizar o salvamento 
+# de arquivos. Para isso, vamos precisar de uma nova classe, que será filha.
+
+from pathlib import Path # importanto path para manipulação caminhos
+from datetime import datetime # esse eu vou usar para adicionar o momento do log.
+from zoneinfo import ZoneInfo # esse será para passar o timezone
+
+# crio a classe e herdo a classe Log
+class LogFileMixin(Log):
+    # sobrescrevendo o método log
+    def log(self, msg):
+        # busca o caminho que o código foi executado e volta para trás uma vez
+        caminho_atual = Path(__file__).parent
+        # denomina um caminho para o arquivo de log
+        arquivo_log = caminho_atual / 'aula228LogFileMixin.txt' 
+
+        # passando o tz de SP
+        tz_SP = ZoneInfo('America/Sao_Paulo')
+        
+        # buscando a hora atual com o tz e formatando
+        agora = datetime.now(tz=tz_SP).strftime('%d/%m/%Y %H:%M:%S')
+
+        # utiliza context maneger para escrita de em arquivos
+        with open(arquivo_log, 'a') as f:
+            f.write(f'{agora} -> {msg}')
+            f.write('\n')
+
+# Agora, quando eu indicar um log, ele vai salvar esse log no arquivo.
+#
+# Para erro:
+LogFileMixin().log_error('Não gostei do nome da variável')
+
+# para sucesso:
+LogFileMixin().log_sucess('aí sim, nome maneiro!')
+
+#########################################
